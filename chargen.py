c3dd4e5c5871b6a370edc49e7418ebe6f0f80f6c
ClockworkOnion <happella@students.uni-marburg.de>
2022-02-12 15:51:29 +0100

    Added lvl, HP, statblock generation...

--- chargen.py
+++ chargen.py
@@ -1,79 +1,98 @@
-import random
+import random, math
+class character_generator():
+    def __init__(self):
+        self.character_info = {"CharLevel" : 1, "Class" : "Fighter"} 
+        self.ability_score_names = ["STR", "CON", "DEX", "WIS", "CHA", "INT"]
+        for score in self.ability_score_names:
+            self.character_info[score] = 10
+        self.hit_dice_table = { # 1st Number : hitdice, 2nd number : HP per level 
+            "Fighter" : [10, 6],
+            "Rogue" : [8, 5],
+            "Wizard" : [6, 4]
+        }
 
-def roll(count, sides):
-    sum = 0
-    for i in range(count):
-        sum += random.randint(1, sides)
-    return sum
+    def calculate_hp(self):
+        level = int(self.character_info["CharLevel"])
+        char_class = self.character_info["Class"]
+        CONmod = int(self.attribute_mod_from_total(self.character_info["CON"]))
+        return self.hit_dice_table[char_class][0] + self.hit_dice_table[char_class][1]*(level-1) + CONmod
 
-def get_attribute_rolls(jobclass):
-    raw_attributes = []
-    ability_scores = dict()
-    for i in range(6):
-        pre_rolls = []
-        for r in range(4): # Roll 4d6 and discard the lowest die
-            pre_rolls.append(roll(1,6))
-        pre_rolls.sort()
-        pre_rolls.reverse()
-        pre_rolls.pop()
-        raw_attributes.append(pre_rolls[0] + pre_rolls[1] + pre_rolls[2])
-    raw_attributes.sort()
-    rand_factor = random.randint(1,5)
-    if (jobclass == "Fighter"):
-        priorities = ["STR", "CON", "DEX", "WIS", "CHA", "INT"]
-    if (jobclass == "Wizard"):
-        priorities = ["INT", "CON", "DEX", "WIS", "CHA", "STR"]
-    if (jobclass == "Rogue"):
-        priorities = ["DEX", "CON", "STR", "WIS", "CHA", "INT"]
-    for index, p in enumerate(priorities):
-        ability_scores[p] = raw_attributes.pop()
-        if (index == rand_factor):
-            print("Random factor " + str(rand_factor) + ". Shuffling remaining values...")
-            random.shuffle(raw_attributes)
-    return ability_scores
+    def roll_dice(self, count, sides):
+        sum = 0
+        for i in range(count):
+            sum += random.randint(1, sides)
+        return sum
 
-def calculate_pointbuy_value(scores):
-    sum = 0
-    for i in scores:
-        if (i == 9):
-            sum = sum + 1
-        if (i == 10):
-            sum = sum + 2
-        if (i == 11):
-            sum = sum + 3
-        if (i == 12):
-            sum = sum + 4
-        if (i == 13):
-            sum = sum + 5
-        if (i == 14):
-            sum = sum + 7
-        if (i >= 15):
-            sum = sum + 9
-    return sum
-
-class Character:
-    def __init__(self, attributes):
-        self.attributes = attributes
-        self.stren = attributes[0]
-        self.con = attributes[1]
-        self.int = attributes[2]
-        self.dex = attributes[3]
-        self.wis = attributes[4]
-        self.cha = attributes[5]
+    def generate_attribute_rolls(self, jobclass):
+        raw_attributes = []
+        ability_scores = dict()
+        for i in range(6):
+            pre_rolls = []
+            for r in range(4): # Roll 4d6 and discard the lowest die
+                pre_rolls.append(self.roll_dice(1,6))
+            pre_rolls.sort()
+            pre_rolls.reverse()
+            pre_rolls.pop()
+            raw_attributes.append(pre_rolls[0] + pre_rolls[1] + pre_rolls[2])
+        raw_attributes.sort()
+        rand_factor = random.randint(1,5)
+        if (jobclass == "Fighter"):
+            priorities = ["STR", "CON", "DEX", "WIS", "CHA", "INT"]
+        if (jobclass == "Wizard"):
+            priorities = ["INT", "CON", "DEX", "WIS", "CHA", "STR"]
+        if (jobclass == "Rogue"):
+            priorities = ["DEX", "CON", "STR", "WIS", "CHA", "INT"]
+        for index, p in enumerate(priorities):
+            ability_scores[p] = raw_attributes.pop()
+            if (index == rand_factor):
+                print("Random factor " + str(rand_factor) + ". Shuffling remaining values...")
+                random.shuffle(raw_attributes)
+        for key in ability_scores:
+            self.character_info[key] = ability_scores[key]
+            self.character_info[key+"mod"] = self.attribute_mod_from_total(ability_scores[key])
+        return ability_scores
     
+    def get_stored_ability_scores(self):
+        print("the values:")
+        for key in self.character_info:
+            print(str(key) + " : "  + str(self.character_info[key]))
 
-class Fighter(Character):
-    name = "Rudolf"
-    lvl = 1
-    def __init__(self, attributes):
-        self.attributes = attributes
-        self.stren = attributes[5]
-        self.con = attributes[4]
-        self.dex = attributes[3]
-        self.wis = attributes[2]
-        self.cha = attributes[1]
-        self.int = attributes[0]
+    def get_pointbuy_value(self): # Works without score parameter
+        scores = []
+        for s in self.ability_score_names:
+            scores.append(int(self.character_info[s]))
+        return self.calculate_pointbuy_value(scores)
 
-def printStatBlock():
-    print(" Hahah")
+    def calculate_pointbuy_value(self, scores):
+        sum = 0
+        for i in scores:
+            if (i == 9):
+                sum = sum + 1
+            if (i == 10):
+                sum = sum + 2
+            if (i == 11):
+                sum = sum + 3
+            if (i == 12):
+                sum = sum + 4
+            if (i == 13):
+                sum = sum + 5
+            if (i == 14):
+                sum = sum + 7
+            if (i == 15):
+                sum = sum + 9
+            if (i >= 16):
+                sum = sum + 12
+        return sum
+
+    def attribute_mod_from_total(self, value):
+        mod = math.floor((value-10)/2)
+        return str(mod) if (mod < 0) else ("+"+ str(mod))
+
+    def create_ASCII_statblock(self):
+        inf = self.character_info
+        hp = self.calculate_hp()
+        headline = "Unnamed  ("  + "Level " + str(inf['CharLevel']) + " " +  self.character_info['Class'] +  ") HP:" + str(hp) + "\\" + str(hp)   + "\n"
+        statsline = f"""STR:{inf['STR']}({inf['STRmod']}); DEX:{inf['DEX']}({inf['DEXmod']}); CON:{inf['CON']}({inf['CONmod']}); WIS:{inf['WIS']}({inf['WISmod']}); INT:{inf['INT']}({inf['INTmod']}); CHA:{inf['CHA']}({inf['CHAmod']});"""
+        return headline + statsline
+
 
--- guiModule.py
+++ guiModule.py
@@ -2,53 +2,52 @@ import wx, math, chargen
 
 class MyFrame(wx.Frame):    
     def __init__(self):
-        super().__init__(parent=None, title='DnD5e Character Creator', size=(500, 500)) # Boilerplate
+        super().__init__(parent=None, title='DnD5e Character Creator', size=(1000, 500)) # Boilerplate
 
         self.info_lines = []
+        self.charagen = chargen.character_generator()
 
         panel = wx.Panel(self)
 
         outer_sizer = wx.BoxSizer(wx.HORIZONTAL)
         right_inner_sizer = wx.BoxSizer(wx.VERTICAL)
-        attributes_sizer = self.create_statblock(panel)
+        attributes_sizer = self.create_ui_base_stat_area(panel)
         outer_sizer.Add(attributes_sizer)
 
-        self.info_text = wx.StaticText(panel, label="Dnd5e Character Creator. Welcome!", style=wx.ALIGN_LEFT)
+        self.info_text = wx.StaticText(panel, label="Dnd5e Character Creator. Welcome!", size=(800, 80), style=wx.ALIGN_LEFT)
         self.info_text.SetFont(self.mono_font)
-        right_inner_sizer.Add(self.info_text, 0, wx.ALL | wx.EXPAND, 5)
+        self.statblock = wx.TextCtrl(panel, size=(200,80), style=wx.TE_MULTILINE, value="Stats will appear here\nAlso another line.")
         
-        self.statblock = wx.TextCtrl(panel, size=(80,80), value="Stats will appear here\nAlso another line.")
         right_inner_sizer.Add(self.statblock, 0, wx.ALL | wx.EXPAND, 5)
+        right_inner_sizer.Add(self.info_text, 0, wx.ALL | wx.EXPAND, 5)
 
         outer_sizer.Add(right_inner_sizer, 0, wx.ALL | wx.EXPAND,5)
         panel.SetSizer(outer_sizer)
         self.Show() 
 
     def set_infotext(self, string):
-        if (len(self.info_lines) > 3):
+        if (len(self.info_lines) > 1):
             self.info_lines.pop(0)
         self.info_lines.append(string)
-
         whole_string = ""
         for l in self.info_lines:
             whole_string += (l+"\n")
         self.info_text.SetLabel(whole_string)
-        
-    def minus_button_triggered(self, event, value):
-        val = int(self.attribute_box_pointers[value].GetValue())
-        new_val:int = (val-1)
-        self.attribute_box_pointers[value].SetValue(str(new_val))
-        self.attribute_mod_pointers[value].SetLabel(self.attribute_mod_from_total(new_val))
-    
-    def plus_button_triggered(self, event, value):
-        val = int(self.attribute_box_pointers[value].GetValue())
-        new_val:int = (val+1)
-        self.attribute_box_pointers[value].SetValue(str(new_val))
-        self.attribute_mod_pointers[value].SetLabel(self.attribute_mod_from_total(new_val))
-    
-    def attribute_mod_from_total(self, value):
-        mod = math.floor((value-10)/2)
-        return str(mod) if (mod < 0) else ("+"+ str(mod))
+
+
+    def plusminus_button_triggered(self, event, stat_name, increment):
+        current_value = int(self.attribute_box_pointers[stat_name].GetValue())
+        new_value:int = (current_value+increment)
+        self.attribute_box_pointers[stat_name].SetValue(str(new_value))
+        self.attribute_mod_pointers[stat_name].SetLabel(self.charagen.attribute_mod_from_total(new_value))
+        self.update_model_ability_values(stat_name, new_value)
+        self.set_infotext("Pointbuy value: " + str(self.charagen.get_pointbuy_value()))
+
+    def update_model_ability_values(self, stat_name, stat_value):
+        self.charagen.character_info[stat_name] = stat_value
+        self.charagen.character_info[stat_name+"mod"]= self.charagen.attribute_mod_from_total(stat_value)
+        self.charagen.get_stored_ability_scores()
+        self.generate_ascii_statblock()
 
     def generate_character(self, event):
         c = self.class_select.GetValue()
@@ -56,26 +55,32 @@ class MyFrame(wx.Frame):
             self.set_infotext("No class selected!")
             return
         self.set_infotext("Creating stats for a " + c)
-        stats = chargen.get_attribute_rolls(c)
+        stats = self.charagen.generate_attribute_rolls(c)
         attributes = ["STR", "DEX", "CON", "WIS", "INT", "CHA"] 
         for a in attributes:
             self.attribute_box_pointers[a].SetValue(str(stats[a]))
-            self.attribute_mod_pointers[a].SetLabel(self.attribute_mod_from_total(stats[a]))
+            self.attribute_mod_pointers[a].SetLabel(self.charagen.attribute_mod_from_total(stats[a]))
 
-        self.set_infotext("Pointbuy value: " + str(chargen.calculate_pointbuy_value(stats.values())))
-        self.calculate_statblock()
+        self.set_infotext("Pointbuy value: " + str(self.charagen.calculate_pointbuy_value(stats.values())))
+        self.generate_ascii_statblock()
     
-    def calculate_statblock(self):
-        self.statblock.SetValue("Hallo")
-
+    def generate_ascii_statblock(self):
+        text = self.charagen.create_ASCII_statblock()
+        self.statblock.SetValue(text)
 
     def on_class_selected(self, event):
         print("Selected " + self.class_select.GetValue())
+        self.charagen.character_info["Class"] = self.class_select.GetValue()
 
     def on_armor_selected(self, event):
         print("Selected " + self.armor_select.GetValue())
+        self.charagen.character_info["Armor"] = self.armor_select.GetValue()
+    
+    def on_level_entered(self, event):
+        self.charagen.character_info["CharLevel"] = self.level_select.GetValue()
+        print("Level set to " + self.level_select.GetValue())
 
-    def create_statblock(self, target_panel):
+    def create_ui_base_stat_area(self, target_panel):
         attributes = ["STR", "DEX", "CON", "WIS", "INT", "CHA"] 
         classes = ["Fighter", "Rogue", "Wizard"]
         armor_types = ["None", "Leather (AC11)", "Studded Leather (AC12)", "Chain Mail (AC16)"]
@@ -87,7 +92,7 @@ class MyFrame(wx.Frame):
         attributes_heading = wx.StaticText(target_panel, label="  Attributes")
         attributes_heading.SetFont(self.mono_font)
         attributes_sizer.Add(attributes_heading)
-        for index, attr in enumerate(attributes):
+        for index, attr in enumerate(attributes): # Ability Scores and their buttons
             inner_sizer = wx.BoxSizer(wx.HORIZONTAL)
             attribute_name_statictext = wx.StaticText(target_panel, label=attr, style=wx.ALIGN_LEFT)
             attribute_name_statictext.SetFont(self.mono_font)
@@ -95,8 +100,8 @@ class MyFrame(wx.Frame):
 
             attr_plusbutton = wx.Button(target_panel, label="+", size=(25,25))
             attr_minusbutton = wx.Button(target_panel, label="-", size=(25,25))
-            attr_plusbutton.Bind(wx.EVT_BUTTON, lambda event, temp=attr: self.plus_button_triggered(event, temp))
-            attr_minusbutton.Bind(wx.EVT_BUTTON, lambda event, temp=attr: self.minus_button_triggered(event, temp))
+            attr_plusbutton.Bind(wx.EVT_BUTTON, lambda event, temp=attr: self.plusminus_button_triggered(event, temp, 1))
+            attr_minusbutton.Bind(wx.EVT_BUTTON, lambda event, temp=attr: self.plusminus_button_triggered(event, temp, -1))
 
             attribute_modifier_statictext = wx.StaticText(target_panel, label="+0", style=wx.ALIGN_LEFT)
             attribute_modifier_statictext.SetFont(self.mono_font)
@@ -118,6 +123,15 @@ class MyFrame(wx.Frame):
 
         attributes_sizer.Add(self.class_select, 0, wx.ALL | wx.EXPAND, 5)
         attributes_sizer.Add(self.armor_select, 0, wx.ALL | wx.EXPAND, 5)
+
+        # Select character level
+        lvl_select_sizer = wx.BoxSizer(wx.HORIZONTAL) 
+        lvl_select_sizer.Add(wx.StaticText(target_panel, label="Lvl", style=wx.ALIGN_LEFT), 0, wx.ALL | wx.EXPAND, 5)
+        self.level_select = wx.TextCtrl(target_panel, size=(25,20), value="1")
+        self.level_select.Bind(wx.EVT_TEXT, self.on_level_entered)
+        lvl_select_sizer.Add(self.level_select, 0, wx.ALL | wx.EXPAND, 5)
+        attributes_sizer.Add(lvl_select_sizer, 0, wx.ALL | wx.EXPAND, 5)
+
         generate_character_button = wx.Button(target_panel, label='Generate Character')
         generate_character_button.Bind(wx.EVT_BUTTON, self.generate_character)
         attributes_sizer.Add(generate_character_button, 0, wx.ALL | wx.CENTER, 5)
